6ceae7a7c71d7ebfd6bc982667da69adc42530de
Klaral <klaral@users.noreply.github.com>
2017-05-30 11:50:11 +0200

    implementerade handle input email function to change state

--- src/App.js
+++ src/App.js
@@ -9,13 +9,26 @@ class App extends Component {
                 namnvalue: "",
                 emailvalue: ""
             }
+            this.handleImputName=this.handleImputName.bind(this);
+            this.handleImputEmail=this.handleImputEmail.bind(this);
+            
+            }
+    handleImputName(event){
+                this.setState({
+                    namevalue: event.target.value
+                })
+    }
+    handleImputEmail(event){
+                this.setState({
+                    emailvalue: event.target.value
+                })
         
     }
   render() {
     return (
       <div className="App">
-        <input placeholder='name'/>
-        <input placeholder='email'/>
+        <input placeholder='name' className='nameinput' value={this.state.namevalue} onChange={this.handleImputName}/>
+        <input placeholder='email' className= 'emailinput' value={this.state.emailvalue} onChange={this.handleImputEmail}/>
       </div>
     );
   }
