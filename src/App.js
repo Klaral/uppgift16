57739ed404a59d146918971cb1198e8d373c9dd0
Klaral <klaral@users.noreply.github.com>
2017-05-30 11:06:18 +0200

    implementerade input element

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {}
-  /*render() {
+class App extends Component {
+  render() {
     return (
       <div className="App">
-        
+        <input placeholder='name'/>
       </div>
     );
   }
-        }*/
+        }
 
 export default App;
