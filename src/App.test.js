614c8453ddd011b5c03b38b1057e7d78b1b0afce
Klaral <klaral@users.noreply.github.com>
2017-05-30 10:57:34 +0200

    ghf

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+test('test input element', () => {
+	var wrapper = shallow(<App />);
+	const greeting = <input placeholder='name'/>;
+	expect(wrapper.contains(greeting)).toBe(true)
+});
